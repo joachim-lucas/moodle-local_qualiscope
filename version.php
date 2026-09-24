5554afbaaa2b452d764f569d594e229e50fc564f
Joachim <joachim.lucas@free.fr>
2026-09-24 08:59:37 +0200

    chore: set release 1.0.0 stable and bump plugin version

--- version.php
+++ version.php
@@ -26,8 +26,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component    = 'local_qualiscope';
-$plugin->version      = 2026092201;
+$plugin->version      = 2026092400;
 $plugin->requires     = 2025040000; // Moodle 5.2.
-$plugin->release      = '1.1.0';
-$plugin->maturity     = MATURITY_ALPHA;
+$plugin->release      = '1.0.0';
+$plugin->maturity     = MATURITY_STABLE;
 $plugin->dependencies = [];
